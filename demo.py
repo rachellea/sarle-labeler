5f8cf97c0df0117761598939695e742bc73c374a
Rachel Draelos <rachel.draelos@gmail.com>
2022-07-14 20:48:39 -0400

    draft updates to demo

--- demo.py
+++ demo.py
@@ -23,21 +23,30 @@
 
 from src import run_sarle, load
 
-def run_rules_demo():
-    """Apply the SARLE-Rules method to the OpenI CXR report dataset."""
-    train_data, test_data, predict_data = load.load_merged_with_style('openi_cxr', 'trainall_testall')
+def run_rules_demo(train_data, test_data, predict_data, dataset_descriptor):
+    """Apply the SARLE-Rules method."""
+    #Demo both options for the ambiguity filter:
     
+
+    #FROM HERE HERE you actually need to reload the dataset before you deploy 
+    #the ambiguity filter different setting because the dataset has been modified
+    #All this reloading is actually SO ANNOYING!!!!! FIX IT
+    #do a deep copy operation in SARLE. update it so SARLE will not modify
+    #the original dataframes. they are deep copied. you can do an assert in this
+    #demo file to show they have not changed.
+    #also finish updating the arguments to run_sarle.generate_labels
+
     #Mark ambiguous findings positive:
-    run_sarle.generate_labels('rules','openi_cxr',False,'pos',True)
+    run_sarle.generate_labels(train_data, test_data, predict_data, 
+                              dataset_descriptor, sarle_variant='rules', 
+                              ambiguities='pos',run_locdis_checks=True)
 
     #Mark ambiguous findings negative:
     run_sarle.generate_labels('rules','openi_cxr',False,'neg',True)
 
 
-def run_hybrid_demo():
+def run_hybrid_demo(train_data, test_data, predict_data):
     """Apply the SARLE-Hybrid method to the OpenI CXR report dataset"""
-    train_data, test_data, predict_data = load.load_merged_with_style('openi_cxr', 'trainall_testall')
-    
     #Mark ambiguous findings positive:
     run_sarle.generate_labels('hybrid','openi_cxr',False,'pos',True)
 
@@ -45,22 +54,54 @@ def run_hybrid_demo():
     run_sarle.generate_labels('hybrid','openi_cxr',False,'neg',True)
 
 
-def run_mini_fake_data_demos():
-    #TODO manually create dataframes here with minimal information
+def load_fake_data():
+    #For legacy reasons, the 'Label' and 'BinLabel' columns are redundant 
+    #with 'h'=0 and 's'=1. They contain the same information but are 
+    #unfortunately both used in the code.
+    #You can easily generate one column from the other programatically.
+    #TODO: eliminate use of the 'Label' column in the future.
+    #The 'Section' column is also there for legacy reasons. You can populate this
+    #entirely with the string 'Findings' or entirely with the string 
+    #'Impression' if you don't want to use this column.
+    train_data = pd.DataFrame(
+        [['s','There is a nodule in the right upper lobe.','report1.txt','Findings','1'],
+        ['s','There is cardiomegaly without pericardial effusion.','report1.txt','Findings','1'],
+        ['h','The lungs are clear.','report2.txt','Findings','0'],
+        ['h','The heart is normal in size.','report2.txt','Findings','0'],
+        ['s','An opacity in the left lower lobe is favored to represent atelectasis vs consolidation.','report3.txt','Findings','1']],
+        columns = ['Label', 'Sentence', 'Filename', 'Section', 'BinLabel'])
+    train_data['BinLabel'] = train_data['BinLabel'].astype(int)
 
-    train_data = pd.DataFrame()
-
-
-    test_data = pd.DataFrame()
+    test_data = pd.DataFrame(
+        [['s','Scattered nodules bilaterally.','report4.txt','Findings','1'],
+        ['s','There is a right pleural effusion.','report4.txt','Findings','1'],
+        ['h','There are no lung abnormalities.','report5.txt','Findings','0']],
+        columns = ['Label', 'Sentence', 'Filename', 'Section', 'BinLabel'])
+    test_data['BinLabel'] = test_data['BinLabel'].astype(int)
     
-
+    #As mentioned in the README, it's okay not to have any predict data. We'll
+    #demo that case here, with an empty df for the predict set.
     predict_data = pd.DataFrame()
+
+    return train_data, test_data, predict_data
+
 
 
 if __name__=='__main__':
-    #Demos on a real dataset, the OpenI CXR reports dataset
-    run_rules_demo()
-    run_hybrid_demo()
+    # #Rules demo on real dataset (OpenI CXR reports)
+    # train_data, test_data, predict_data = load.load_merged_with_style('openi_cxr', 'trainall_testall')
+    # run_rules_demo(train_data, test_data, predict_data)
+    
+    # #Hybrid demo on real dataset (OpenI CXR reports)
+    # #Reload the data because running SARLE modifies the data, so the data
+    # #previously loaded is no longer fresh
+    # train_data, test_data, predict_data = load.load_merged_with_style('openi_cxr', 'trainall_testall')
+    # run_hybrid_demo(train_data, test_data, predict_data)
 
-    #Demos on fake data
-    run_mini_fake_data_demos()
+    #Rules demo on fake dataset
+    train_data, test_data, predict_data = load_fake_data()
+    run_rules_demo(train_data, test_data, predict_data)
+    
+    #NHybrid demo on fake dataset
+    train_data, test_data, predict_data = load_fake_data()
+    run_hybrid_demo(train_data, test_data, predict_data)
